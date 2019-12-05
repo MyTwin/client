9c1142a205ac65cd79b479f3b20697c0801adea1
Immanuel Pandiangan <nuelpan@gmail.com>
2019-12-05 19:13:37 +0700

    add imageDisplay

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
-<div>
-  <h1>{{ message }}</h1>
-  <!-- <div class="fb-quote">Ini adalah quote</div> -->
-  <div><a :href="shareFacebook" target="_blank"><span><i class="fab fa-facebook" style="margin-right: 5px;"></i></span></a></div>
-</div>
+  <div>
+    <div><a :href="shareFacebook" target="_blank"><span><i class="fab fa-facebook" style="margin-right: 5px;"></i></span></a></div>
+    <imageDisplay :srcUser="userImage" :srcResult="resultImage"></imageDisplay>
+    <!-- <listPost :posts="listPost"></listPost> -->
+  </div>
 </template>
 
 <script>
+import imageDisplay from './components/imageDisplay'
+// import listPost from './components/listPost'
+
 export default {
   data() {
     return {
       message: 'Hello world',
       linkImage: 'https%3A%2F%2Fstorage.googleapis.com%2Fecommercebucket.danangbahari.com%2F1575580332867ayam.jpg',
-      desc: 'ini adalah ayam yang enak sekali'
+      desc: 'ini adalah ayam yang enak sekali',
+      userImage: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+      resultImage: 'https://i.dailymail.co.uk/1s/2019/10/29/14/20320992-7625529-Cara_a_five_year_old_125lb_Bengal_tiger_has_been_fitted_with_a_g-a-8_1572358357911.jpg',
+      listPost: [
+        {
+          id:1,
+          userImage: 'https://picsum.photos/400/520',
+          resultImage: 'https://picsum.photos/400/520'
+        },
+        {
+          id:2,
+          userImage: 'https://picsum.photos/400/520',
+          resultImage: 'https://picsum.photos/400/520'
+        },
+        {
+          id:3,
+          userImage: 'https://picsum.photos/400/520',
+          resultImage: 'https://picsum.photos/400/520'
+        },
+        {
+          id:4,
+          userImage: 'https://picsum.photos/400/520',
+          resultImage: 'https://picsum.photos/400/520'
+        }
+      ]
     };
+  },
+  components: {
+    imageDisplay,
+    // listPost
   },
   computed:{
     shareFacebook(){
@@ -23,8 +54,12 @@ export default {
 }
 </script>
 
-<style scoped>
-a{
-  text-decoration: none;
-}
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  a{
+    text-decoration: none;
+  }
 </style>
---
+++ src/components/imageDisplay.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="box-image">
+        <div class="image-wrapper">
+            <img :src="srcUser">
+        </div>
+        <div class="image-wrapper">
+            <img :src="srcResult">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['srcUser', 'srcResult'],
+}
+</script>
+
+<style scoped>
+    .box-image {
+        height: 80vh;
+        width: 90vw;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-evenly;
+    }
+    .image-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.6);
+        cursor: pointer;
+    }
+    .image-wrapper img {
+        width: 400px;
+        flex-shrink: 0;
+        min-width: 100%;
+        min-height: 100%    
+    }
+</style>
