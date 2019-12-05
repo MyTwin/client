5b88b8d205ee65ca8a798925c4de9cca49cd421b
EddyPermana22 <eddy.permana.sari@gmail.com>
2019-12-05 17:48:48 +0700

    initial commit untuk membuat component button upload

---
+++ src/components/buttonUpload.js
@@ -0,0 +1,13 @@
+<template>
+    <p>Canvas</p>
+    <Pixel />
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        components: {
+            Pixel
+        }
+    }
+</script>
