8c0f186858e34be7ef6985f612f661756bcb3d93
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-06 09:39:45 +0700

    sharing facebook done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
+<div>
   <h1>{{ message }}</h1>
+  <!-- <div class="fb-quote">Ini adalah quote</div> -->
+  <div><a :href="shareFacebook" target="_blank"><span><i class="fab fa-facebook" style="margin-right: 5px;"></i></span></a></div>
+</div>
 </template>
 
 <script>
@@ -7,10 +11,20 @@ export default {
   data() {
     return {
       message: 'Hello world',
+      linkImage: 'https%3A%2F%2Fstorage.googleapis.com%2Fecommercebucket.danangbahari.com%2F1575580332867ayam.jpg',
+      desc: 'ini adalah ayam yang enak sekali'
     };
   },
-};
+  computed:{
+    shareFacebook(){
+      return `https://www.facebook.com/sharer/sharer.php?u=${this.linkImage}&quote=${this.desc}`
+    }
+  }
+}
 </script>
 
 <style scoped>
+a{
+  text-decoration: none;
+}
 </style>
