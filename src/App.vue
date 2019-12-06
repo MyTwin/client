1b2e060156931b27126266e63f6e695e6d20330c
EddyPermana22 <eddy.permana.sari@gmail.com>
2019-12-06 10:16:17 +0700

    finnish button upload component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <!-- <h1>{{ message }}</h1>
+    <button-upload @asdasd="uploadImage" v-show="showButtonUpload"></button-upload>
+    <BlockUI :message="blockUIData['msg']" :url="blockUIData['url']" v-if="blockUIData['display']"></BlockUI> -->
+    <upload-image></upload-image>
+  </div>
 </template>
 
 <script>
+
+import Vue from 'vue'
+import BlockUI from 'vue-blockui'
+import loadingImage from './assets/logo-hacktiv8-animation.gif'
+import buttonUpload from './components/buttonUpload'
+import uploadImage from './components/uploadImage'
+
+Vue.use(BlockUI)
+
 export default {
   data() {
     return {
-      message: 'Hello world',
+      showButtonUpload: true,
+      blockUIData: {
+        display: false,
+        msg: 'Please Wait...',
+        url: loadingImage
+      }
     };
   },
+  components: {
+    buttonUpload,
+    uploadImage
+  },
+  methods: {
+   uploadImage: function(){
+     this.showButtonUpload = !this.showButtonUpload
+     this['blockUIData']['display'] = true
+   }
+  }
 };
 </script>
 
---
+++ src/components/uploadAnimation.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="upload-btn-wrapper mx-4 my-4">
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      
+    }
+  },
+  methods: {
+    uploadImage: function(){
+      // alert('masuk')
+      this.$emit('asdasd','test')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .upload-btn-wrapper {
+    position: relative;
+    overflow: hidden;
+    display: inline-block;
+  }
+
+  .btn {
+    border: 2px solid gray;
+    color: gray;
+    background-color: white;
+    padding: 8px 20px;
+    border-radius: 8px;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .upload-btn-wrapper input[type=file] {
+    font-size: 100px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0;
+  }
+</style>
