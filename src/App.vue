6cb8e6b73b7759ef86ade3769eff5d4fa8a00bcf
Danang Putra Bahari <danangbahari89@gmail.com>
2019-12-06 07:47:30 +0700

    facebookmbutton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
+<div>
   <h1>{{ message }}</h1>
+  <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://storage.googleapis.com/ecommercebucket.danangbahari.com/1575580332867ayam.jpg" class="fb-xfbml-parse-ignore">Bagikan</a></div>
+  </div>
+</div>
 </template>
 
 <script>
